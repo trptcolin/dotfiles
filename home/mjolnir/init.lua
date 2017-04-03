73060de64795aefb781aee200a6709f2c3952401
Colin Jones <trptcolin@gmail.com>
2017-04-03 17:34:48 -0500

    Initial commit

---
+++ home/mjolnir/init.lua
@@ -0,0 +1,29 @@
+local hotkey = require "mjolnir.hotkey"
+local alert = require "mjolnir.alert"
+local grid = require "mjolnir.sd.grid"
+
+grid.MARGINX = 0
+grid.MARGINY = 0
+grid.GRIDWIDTH = 4
+
+local mash = {"cmd", "alt", "ctrl"}
+local mashshift = {"cmd", "alt", "shift"}
+
+hotkey.bind(mash, '=', function() grid.adjustwidth( 1) end)
+hotkey.bind(mash, '-', function() grid.adjustwidth(-1) end)
+
+hotkey.bind(mash, 'M', grid.maximize_window)
+
+hotkey.bind(mash, 'N', grid.pushwindow_nextscreen)
+hotkey.bind(mash, 'P', grid.pushwindow_prevscreen)
+
+hotkey.bind(mash, 'J', grid.pushwindow_down)
+hotkey.bind(mash, 'K', grid.pushwindow_up)
+hotkey.bind(mash, 'H', grid.pushwindow_left)
+hotkey.bind(mash, 'L', grid.pushwindow_right)
+
+hotkey.bind(mash, 'U', grid.resizewindow_taller)
+hotkey.bind(mash, 'O', grid.resizewindow_wider)
+hotkey.bind(mash, 'I', grid.resizewindow_thinner)
+
+alert.show "Mjolnir, at your service."

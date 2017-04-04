33cfb4e68a1f1f6f52a408c5959e2a3b4e2bc090
Colin Jones <trptcolin@gmail.com>
2017-04-04 13:58:40 -0500

    Improve prompt

--- home/zshrc.d/prompt.zsh
+++ home/zshrc.d/prompt.zsh
@@ -2,19 +2,20 @@ autoload -U colors && colors
 autoload -Uz vcs_info
 
 zstyle ':vcs_info:*' stagedstr '%F{28}•%f'
-zstyle ':vcs_info:*' unstagedstr '%F{11}•%f'
+zstyle ':vcs_info:*' unstagedstr '%F{1}•%f'
 zstyle ':vcs_info:*' check-for-changes true
 zstyle ':vcs_info:(sv[nk]|bzr):*' branchformat '%b%F{1}:%F{11}%r'
 zstyle ':vcs_info:*' enable git svn
-precmd () {
-    if [[ -z $(git ls-files --other --exclude-standard 2> /dev/null) ]] {
-        zstyle ':vcs_info:*' formats ' [%b%c%u]'
-    } else {
-        zstyle ':vcs_info:*' formats ' [%b%c%u%F{blue}•%f]'
-    }
-
-    vcs_info
+zstyle ':vcs_info:*' formats ' [%F{2}%b%f%c%u%f]'
+zstyle ':vcs_info:git*+set-message:*' hooks git-untracked
++vi-git-untracked(){
+    if [[ $(git rev-parse --is-inside-work-tree 2> /dev/null) == 'true' ]] && \
+        git status --porcelain | grep '??' &> /dev/null ; then
+        hook_com[staged]+='%F{3}•%f'
+    fi
 }
 
+precmd () { vcs_info }
+
 setopt prompt_subst
-PROMPT='[%n:%c%{$reset_color%}]${vcs_info_msg_0_} %(?/%F{blue}/%F{red})% %(!.#.$) %{$reset_color%}'
+PROMPT='[%F{0}%n:%F{4}%c%{$reset_color%}]${vcs_info_msg_0_} %(?/%F{blue}/%F{red})% %(!.#.$) %{$reset_color%}'

ac6099c043bea4b71348f911ed4a87a6c2c8295b
Colin Jones <trptcolin@gmail.com>
2018-06-22 13:53:51 -0500

    Make python3 the default

--- home/zshrc.d/python.zsh
+++ home/zshrc.d/python.zsh
@@ -1,4 +1,4 @@
 source /usr/local/bin/virtualenvwrapper.sh
-export PIP_REQUIRE_VIRTUALENV=true
+export PIP_REQUIRE_VIRTUALENV=false
 source $(brew --prefix autoenv)/activate.sh
-export PATH=$PATH:$HOME/Library/Python/3.6/bin
+export PATH=$PATH:"$(python3 -m site --user-base)/bin":"$(python2 -m site --user-base)/bin"

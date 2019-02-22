f9e50c21d4cd2af8cae0ea646a19437eae406711
Colin Jones <trptcolin@gmail.com>
2019-02-22 08:32:40 -0600

    Get rid of autoenv
    It was breaking `cd -` (changing to the previous directory).

--- home/zshrc.d/python.zsh
+++ home/zshrc.d/python.zsh
@@ -1,4 +1,3 @@
 source /usr/local/bin/virtualenvwrapper.sh
 export PIP_REQUIRE_VIRTUALENV=false
-source $(brew --prefix autoenv)/activate.sh
-export PATH=$PATH:"$(python3 -m site --user-base)/bin":"$(python2 -m site --user-base)/bin"
+export PATH=/usr/local/opt/python/libexec/bin:$PATH:"$(python3 -m site --user-base)/bin":"$(python2 -m site --user-base)/bin"
